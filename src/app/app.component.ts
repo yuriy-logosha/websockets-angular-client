112fd8f2964a205c82b68ad1f5ffc154663fe1a1
yura <yuriy.logosha@gmail.com>
2019-10-19 10:42:25 +0300

    Enable SEND functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,229 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { ClientsService } from './clients.service';
+import {WebsocketService} from './websocket';
+import { WS } from './websocket.events';
+import { Observable } from 'rxjs';
+import { ImageService } from './image.service';
+import { IWsUser, IWsMessage } from './websocket/websocket.interfaces';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'websockets-angular-client';
+
+  // imgUrl: string = 'https://picsum.photos/200/300/?random';
+  imgUrl = 'http://localhost.zyxel.com/screen?id=7';
+  imageToShow: any;
+  isImageLoading: boolean;
+
+  // clientsSrvs = new ClientsService();
+
+  commands = [
+    {name: 'restart-thread', type: 'command'},
+    {name: 'clean-queue', type: 'command'},
+    {name: 'refresh', type: 'command'},
+    {name: 'help-clan', type: 'command'},
+    {name: 'reload-data', type: 'command'},
+    {name: 'prize', type: 'command'},
+    {name: 'install', type: 'command'},
+    {name: 'myerrors-check', type: 'command'},
+    {name: 'restartserver', type: 'command'},
+    {name: 'restartlauncher', type: 'command'},
+    {name: 'exercises 1', type: 'command'},
+    {name: 'exercises 2', type: 'command'},
+    {name: 'exercises 3', type: 'command'},
+    {name: 'rss-to "rig" food 10', type: 'incremental'},
+    {name: 'rss-to "rig" lumber 10', type: 'incremental'},
+    {name: 'rss-to "rig" iron 10', type: 'incremental'},
+    {name: 'rss-to "rig" stone 10', type: 'incremental'},
+    {name: 'rss-to "rig" silver 10', type: 'incremental'},
+    {name: '', type: ''}
+  ];
+
+  public logs = [];
+
+  public messages$: Observable<IWsUser[]>;
+
+  public messages: IWsUser[];
+
+  constructor(private wsService: WebsocketService, private imageService: ImageService) { }
+
+  public ngOnInit() {
+    $(document).ready(() => {
+      $('.ui-layout-container').layout({ applyDefaultStyles: true });
+    });
+
+    // get messages
+    this.messages$ = this.wsService.on<IWsUser[]>('users');
+
+    this.messages$.subscribe(msg => {
+      this.messages = msg;
+      console.log(msg);
+
+      this.messages.forEach(usr => {
+        if (usr.raw_status) {
+          usr.status = JSON.parse(usr.raw_status);
+        } else if (typeof usr.status === 'string' ) {
+          usr.status = JSON.parse(usr.status);
+        }
+        if (usr.status) {
+          usr.status.history = this.reverseArray(usr.status.history);
+          usr.status.queue = this.reverseArray(usr.status.queue);
+        }
+      });
+
+    });
+  }
+
+  createImageFromBlob(image: Blob) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      this.imageToShow = reader.result;
+    }, false);
+
+    if (image) {
+      reader.readAsDataURL(image);
+    }
+  }
+
+  getImageFromService() {
+    this.isImageLoading = true;
+    $.ajax({
+      url: this.imgUrl,
+      beforeSend( xhr ) {
+        xhr.setRequestHeader ('Authorization', 'Basic cm9vdDohU2hhbWFuaXNoZTIwMTc=');
+      },
+      success: ( data ) => {
+        this.createImageFromBlob(data);
+        this.isImageLoading = false;
+        // if ( console && console.log ) {
+        //   console.log( 'Sample of data:', data.slice( 0, 100 ) );
+        // }
+      },
+      error: (error) => {
+        this.isImageLoading = false;
+        this.imageToShow = '';
+        console.log(error);
+      }
+    });
+    //
+    // this.imageService.getImage(this.imgUrl).subscribe(data => {
+    //   this.createImageFromBlob(data);
+    //   this.isImageLoading = false;
+    // }, error => {
+    //   this.isImageLoading = false;
+    //   this.imageToShow = '';
+    //   console.log(error);
+    // });
+  }
+
+  reverseArray(arr: any[]) {
+    const newArray = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+      newArray.push(arr[i]);
+    }
+    return newArray;
+  }
+
+  send(): void {
+    const uuid = $('#uuid').val();
+    if (uuid === '') {
+      alert('No user! Please select user.');
+    } else {
+      const msg = {type: 'command', uuid, command: $('#cmd').val() + ($('#val').is(':visible') ? (' ' + $('#val').val()) : '')};
+      console.log(msg);
+      this.wsService.send(WS.SEND.TYPE, msg);
+      this.addLog('Send to ' + $('#user-name').text() + ':' + msg);
+    }
+  }
+
+  sendToAll(): void {
+    $.each($('#users a'), (idx, btn) => {
+      this.wsService.send(WS.SEND.TYPE, {type: 'command', uuid: btn.id, command: $('#cmd').val() + ($('#val').is(':visible') ? (' ' + $('#val').val()) : '')});
+      console.log(btn.id);
+    });
+  }
+
+  setCmd(cmd, type: string): void {
+    if (type.includes('incremental')) {
+      $('.addit').show();
+      $('#val').show();
+      if ($('#cmd').val().length > 0) {
+        const cmdMain = $('#cmd').val();
+        const targetMain = cmd.split(' ');
+        const inc = targetMain.pop();
+
+        if (cmdMain !== targetMain.join(' ')) {
+          $('#cmd').val(targetMain.join(' '));
+          $('#val').val(parseInt(inc));
+        } else {
+          $('#val').val(parseInt($('#val').val()) + parseInt(inc));
+        }
+      } else {
+        const targetMain = cmd.split(' ');
+        const wasBefore = targetMain.pop();
+        $('#cmd').val(targetMain.join(' '));
+        $('#val').val(parseInt(wasBefore));
+      }
+    } else {
+      $('#val').hide();
+      $('.addit').hide();
+      $('#cmd').val(cmd);
+    }
+  }
+
+  addLog(msg): void {
+    // this.logs.push(msg);
+  }
+
+  setUpdateInterval(): void {
+    if ($('#reload')[0].checked === true) {
+      this.wsService.connect();
+    } else {
+      if (this.wsService) {
+        this.wsService.ngOnDestroy();
+      }
+    }
+  }
+
+
+  reload(): void {
+    // console.log(this.wsService.getSettings()['uuid']);
+    // this.wsService.send(WS.SEND.TYPE, {type: 'command', uuid: this.wsService.getSettings()['uuid'], command: 'status'});
+    $.each($('#users a'), (idx, btn) => {
+      this.wsService.send(WS.SEND.TYPE, {type: 'command', uuid: btn.id, command: 'status'});
+      console.log(btn.id);
+      // $('#users > a.active').attr('id');$('#'+act).tab('show')
+    });
+  }
+
+  selectUser(id, name): void {
+    $('#user-name').text(name);
+    $('#uuid').val(id);
+    this.getImageFromService();
+  }
+
+  inc(): void {
+    $('#val').val(parseFloat($('#val').val()) + 10);
+  }
+
+  dec(): void {
+    const oldValue = $('#val').val();
+    if (oldValue > 0) {
+      $('#val').val(parseFloat(oldValue) - 10);
+    }
+  }
+
+  generateTime(time: number): string {
+    if (!time) {return; }
+    const offset = 3;
+    return new Date(time * 1000 + (3600000 * offset)).toISOString().slice(11, -1);
+  }
+
 }
